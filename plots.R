e339159189293988194882f4e2bb0f87bab1acd7
wojaw <wojaw83@gmail.com>
2014-09-08 00:35:40 +0200

    second commit - adding plots

---
+++ plots.R
@@ -0,0 +1,61 @@
+data = read.csv2('data.csv', stringsAsFactors=F, na.strings = "?", dec='.',
+      colClasses=c(rep('character',2), rep('numeric',7)))
+
+# plot1
+png('plot1.png', width=480, height=480)
+hist(data$Global_active_power, col='red',
+     main='Global Activde Power', xlab='Global Activde Power (kilowatts)',
+     xlim=c(0,7.5), breaks=seq(0,7.5,0.5))
+dev.off()
+
+# plot2
+png('plot2.png', width=480, height=480)
+plot(data$Global_active_power, xlab=NA, ylab='Global Activde Power (kilowatts)', type='l', axes=F)
+box('plot')
+plot2_days = c(which(data$Time == '00:00:00'),nrow(data))
+axis(1, at=plot2_days, labels=c('Thu','Fri','Sat'))
+axis(2)
+dev.off()
+
+# plot3
+png('plot3.png', width=480, height=480)
+plot(data$Sub_metering_1, axes=F, xlab=NA, ylab='Energy sub metering', type='l')
+lines(data$Sub_metering_2, col=2)
+lines(data$Sub_metering_3, col=4)
+box('plot')
+axis(1, at=plot2_days, labels=c('Thu','Fri','Sat'))
+axis(2)
+legend('topright', lty=1, col=c(1,2,4), legend=paste0('Sub_metering', 1:3))
+dev.off()
+
+
+# plot4
+png('plot4.png', width=480, height=480)
+par(mfrow=c(2,2))
+
+plot(data$Global_active_power, xlab=NA, ylab='Global Activde Power', type='l', axes=F)
+box('plot')
+plot2_days = c(which(data$Time == '00:00:00'),nrow(data))
+axis(1, at=plot2_days, labels=c('Thu','Fri','Sat'))
+axis(2)
+
+plot(data$Voltage, axes=F, xlab='datetime', ylab='Voltage', type='l')
+box('plot')
+axis(1, at=plot2_days, labels=c('Thu','Fri','Sat'))
+axis(2)
+
+plot(data$Sub_metering_1, axes=F, xlab=NA, ylab='Energy sub metering', type='l')
+lines(data$Sub_metering_2, col=2)
+lines(data$Sub_metering_3, col=4)
+box('plot')
+axis(1, at=plot2_days, labels=c('Thu','Fri','Sat'))
+axis(2)
+legend('topright', lty=1, col=c(1,2,4), legend=paste0('Sub_metering', 1:3), bty='n', cex=0.75)
+
+plot(data$Global_reactive_power, xlab='datetime', ylab='Global Rectivde Power', type='l', axes=F)
+box('plot')
+plot2_days = c(which(data$Time == '00:00:00'),nrow(data))
+axis(1, at=plot2_days, labels=c('Thu','Fri','Sat'))
+axis(2)
+
+dev.off()
